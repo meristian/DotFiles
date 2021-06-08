315282c01b692350fc35328a9e793b82e661405a
Cristian <overrideryt@gmail.com>
2021-06-08 19:55:04 +0000

    removed .vim/ and made easy to install in two runs

--- install.sh
+++ install.sh
@@ -3,19 +3,25 @@ ln -nfs ~/.DotFiles/.tmux.conf ~
 ln -nfs ~/.DotFiles/.vim ~
 ln -nfs ~/.DotFiles/.viminfo ~
 tmux source-file ~/.tmux.conf
-#sudo apt-get install python3-autopep8
+mkdir ~/.config
+mkdir ~/.config/nvim/
+touch ~/.config/nvim/init.vim
+echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after" >> ~/.config/nvim/init.vim
+echo "let &packpath=&runtimepath " >> ~/.config/nvim/init.vim
+echo "source ~/.vimrc" >> ~/.config/nvim/init.vim
 
+pip3 install black
 pip3 install --user --upgrade neovim
+
+# Install plug
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Then we have to do :PlugInstall
 # Then go to .vim/Plugged/YouCompleteMe/ and run python3 install.py 
+nvim -c ':PlugInstall' -c "q"  -c "wq" ~/.vimrc
 
+sudo python3  ~/.vim/plugged/YouCompleteMe/install.py  
 
-# Other programs
-
-# Yabai
-brew install koekeishiya/formulae/yabai
-brew install koekeishiya/formulae/skhd
-brew services start yabai 
-brew services start skhd
 
---
+++ install_linux.sh
@@ -0,0 +1,4 @@
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install neovim
+
